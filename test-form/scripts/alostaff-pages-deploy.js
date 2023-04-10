965673c61f7d72c400867a037a4acef2af1b757f
Vanessa Ulloa <vanessa.ulloa@bsci.com>
2023-04-10 00:37:33 -0700

    move incoming pages and change branch name in script

--- test-form/scripts/alostaff-pages-deploy.js
+++ test-form/scripts/alostaff-pages-deploy.js
@@ -3,19 +3,19 @@ const execa = require("execa");
 const fs = require("fs");
 (async () => {
   try {
-    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    await execa("git", ["checkout", "--orphan", "alostaff-pages"]);
     // eslint-disable-next-line no-console
     console.log("Building started...");
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
-    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
-    console.log("Pushing to gh-pages...");
-    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "alostaff-pages"]);
+    console.log("Pushing to alostaff-pages...");
+    await execa("git", ["push", "origin", "HEAD:alostaff-pages", "--force"]);
     await execa("rm", ["-r", folderName]);
     await execa("git", ["checkout", "-f", "master"]);
-    await execa("git", ["branch", "-D", "gh-pages"]);
+    await execa("git", ["branch", "-D", "alostaff-pages"]);
     console.log("Successfully deployed, check your settings");
   } catch (e) {
     // eslint-disable-next-line no-console
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-<<<<<<< HEAD
-  publicPath: process.env.NODE_ENV == 'production' ? '/' : '/',
-=======
   publicPath: process.env.NODE_ENV == 'production' ? '/' : '/test-form/',
->>>>>>> 7806d43 (add deploy script)
   transpileDependencies: true
 })
